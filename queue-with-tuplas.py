4e732c8478e87ecaf4ed8d6aba81562db24b21c6
Arthur Fabricyo <142946955+ElFabrica@users.noreply.github.com>
2026-04-21 11:41:03 -0300

    Delete queue-with-tuplas.py

--- queue-with-tuplas.py
+++
@@ -1,9 +0,0 @@
-from queue import PriorityQueue
-
-fila = PriorityQueue()
-fila.put((2, "Ler livro"))
-fila.put((2, "Ler livro"))
-fila.put((2, "Ler livro"))
-fila.put((2, "Ler livro"))
-while not fila.empty():
-    fila.get()
